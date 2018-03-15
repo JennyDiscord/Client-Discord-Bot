51bc279a942ef4f831f23b5455ff72896382ff3c
JennyDiscord <37352986+JennyDiscord@users.noreply.github.com>
2018-03-15 17:57:35 -0400

    Install.bat
    I am Arnold-yui-dib if you are wondering

---
+++ Install.bat
@@ -0,0 +1,7 @@
+@color a
+pip3 install discord
+@echo If you want to install an extra python module please press enter. If you don't please close...
+pause
+pip3 install asyncio
+@echo Done, you may start with your bot writen in Discord.py
+pause
